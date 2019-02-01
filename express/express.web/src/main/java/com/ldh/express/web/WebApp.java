1b5b8add30fb6422ac3bfd3dcb81b575347af7cb
ldh <525917388@qq.com>
2019-02-01 16:34:12 +0800

    first add

---
+++ express/express.web/src/main/java/com/ldh/express/web/WebApp.java
@@ -0,0 +1,19 @@
+package com.ldh.express.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * web项目启动类
+ * @author Li Dehuan
+ * @date 2019年2月1日
+ *
+ */
+@SpringBootApplication
+public class WebApp {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(WebApp.class, args);
+	}
+}
